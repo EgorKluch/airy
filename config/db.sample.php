726c5cda4e81a1a61a557a0d03fa0448b746f7e0
EgorKluch <EgorKluch@gmail.com>
2014-06-10 14:40:52 +0700

    Add EgorKluch\MysqlQueryBuilder package

---
+++ config/db.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author EgorKluch (EgorKluch@gmail.com)
+ * @date: 10.06.2014
+ */
+
+return array(
+  'db' => 'my_db',
+  'user' => 'root',
+  'pass' => 'myPass',
+  'host' => 'localhost'
+);
--- index.php
+++ index.php
@@ -4,9 +4,15 @@
  * @date: 09.06.2014
  */
 
+use Slim\Slim;
+use EgorKluch\MysqlQueryBuilder;
+
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new Slim();
+
+$conf = require __DIR__ . '/config/db.php';
+$app->db = new MysqlQueryBuilder($conf['db'], $conf['user'], $conf['pass'], $conf['host']);
 
 $app->get('/hello/:name', function ($name) {
   echo "Hello, $name";
